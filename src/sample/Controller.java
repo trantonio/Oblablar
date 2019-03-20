22c9e6f5d27eff208aa800571dc99f65f6823f06
iam53837857 <trantoniio@gmail.com>
2019-03-20 19:25:49 +0100

    Estructura basica

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,12 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,13 +14,27 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
 
+
+//    @FXML private TextArea taWords;
+//    public void btCheck(ActionEvent event){
+//
+//    }
+    @FXML private Text txWords;
+    public void btCheck(ActionEvent event){
+        txWords.setText("Words");
+//        taWords.setText("Area");
+
+    }
+
+    //Le da la funcion de cerrar la ventana al boton
+    @FXML
+    private void btClose(ActionEvent event) {
+        Platform.exit();
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
-    @FXML
-    private void btClose(ActionEvent event)
-    {
-        System.out.close();
-    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -20,16 +23,27 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-
-//        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 1024 , 700));
         primaryStage.setResizable(false);
+        Text tx = new Text();
+        Button bt = new Button("btCheck");
+
+        bt.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                tx.setText("Accepted");
+            }
+        });
+
+
         primaryStage.show();
 
-        Text t = TextBuilder.create().text("This is a text sample").build();
-        t.setText("This is a text sample");
-        t.setFont(Font.font ("Verdana", 20));
-        t.setFill(Color.RED);
+
+
+//        Text t = TextBuilder.create().text("This is a text sample").build();
+//        t.setText("This is a text sample");
+//        t.setFont(Font.font ("Verdana", 20));
+//        t.setFill(Color.RED);
 
 
 
